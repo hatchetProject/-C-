dfa33404b4e985ba57e2ff92b247beef93852436
Jerry <hatchet25@sjtu.edu.cn>
2018-01-12 23:26:32 +0800

    提交文件

---
+++ lancome/a.py
@@ -0,0 +1,4 @@
+f=open('lancome.txt','r')
+tmp2=f.readline().split('\t')
+print tmp2[0].decode('utf8')
+print type(tmp2[0].decode('utf8'))
---
+++ ysl/a.py
@@ -0,0 +1,4 @@
+f=open('ysl.txt','r')
+tmp2=f.readline().split('\t')
+print tmp2[0].decode('gbk')
+print type(tmp2[0].decode('gbk'))
